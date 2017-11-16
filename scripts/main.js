0e8de2677a161c529db0e69525e63231e36cdc54
azou-nisum-com <azou@nisum.com>
2017-11-16 15:49:58 -0800

    Initialize initial project

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+$('#size-selection').show();
+
+$('#facets').on('change', function() {
+
+  $('#size-selection').hide();
+  $('#color-selection').hide();
+  $('#quantity-selection').hide();
+
+  if (this.value == 'Size') {
+    $('#size-selection').show();
+  } else if (this.value == 'Color') {
+    $('#color-selection').show();
+  } else {
+    $('#quantity-selection').show();
+  }
+})
+
+$('#next').on('click', function() {
+  $('#shipaddress').show();
+})
+
+$('#finish').on('click', function() {
+  $('#analyticsinfo').show();
+  // Display the information picked up above.
+})
+
