2a7f4e8fd28e60a05f79810ae19cab3970b62b87
azou-nisum-com <azou@nisum.com>
2017-11-17 14:25:27 -0800

    Fix Handlebars iteration of radio buttons

--- scripts/handlebars-helpers/helpers.js
+++ scripts/handlebars-helpers/helpers.js
@@ -2,8 +2,13 @@
 Handlebars.registerHelper('timesQuantity', function(items, options) {
   var accum = '';
 
-  for(var i = 0; i < items; ++i)
-    accum += options.fn(i+1);
+  for(var i = 0; i < items; ++i) {
+    if (i === 0) {
+      accum += '<input type="radio" name="sizes" value={{this}} checked>1'
+    } else {
+      accum += options.fn(i+1);
+    }
+  }
   return accum;
 
 });
